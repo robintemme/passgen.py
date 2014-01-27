28fc85a213c5d3a22687afa48e2271bc529cf9f8
zwerch <zwerch1337@googlemail.com>
2014-01-27 05:56:50 +0100

    finished core, everything working

--- passgen.py
+++ passgen.py
@@ -1,31 +1,45 @@
-import subprocess
-import random
+# imports
+from subprocess import Popen, PIPE
+from random import choice
+
+# global variables and constants
+pw_length = 8
+charset_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+charset_lower = "abcdefghijklmnopqrstuvwxyz"
+charset_num = "1234567890"
+pw_charset = charset_upper + charset_lower + charset_num
 
 # only tested for osx yet
 def set_clipboard_data(data: str):
-    p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
+    p = Popen(['pbcopy'], stdin=PIPE)
     p.stdin.write(data.encode())
     p.stdin.close()
     retcode = p.wait()
 
 def gen_pw() -> str:
-    return "NoGeneratedPass"
+    pw = ""
+    for i in range(pw_length):
+        pw += choice(pw_charset)
+    return pw
 
 while True:
-    print("""This is a simple password generator.
-            Just press 'Any Key' as long as you want
-            to have a password generated. When you like
-            to use the password you saw, press ^C (ctrl-c)
-            and the password will be copied to your clipboard.""")
+    print("This is a simple password generator.", \
+            "Just press 'Enter' as long as you want", \
+            "to have a password generated. When you like", \
+            "to use the password you saw, press ^C (ctrl-c)", \
+            "and the password will be copied to your clipboard.", \
+            "Press ^D (ctrl-c) to exit without copying.", sep="\n")
     try:
         last_pw = gen_pw()
-        input(last_pw + "\n")
+        input("\n" + last_pw + "\n")
     except KeyboardInterrupt:
         try:
             set_clipboard_data(last_pw)
             print("\nSuccessfully copied.")
             break
         except:
-            raise
             print("\nError copying to clipboard.")
             break
+    except EOFError:
+        print("Exited without copying.")
+        break
